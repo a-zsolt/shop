75c66c56b65c2425fc30a9a1e9dc99baad901899
GerzsonUwU <zsolt.andorko.111@gmail.com>
2022-10-04 00:04:40 +0200

    first commit

---
+++ js/store.js
@@ -0,0 +1,43 @@
+const modeicon = document.getElementsByClassName("modeswitchicon")
+const sidenav = document.getElementsByClassName("sidenav")
+const showcase = document.getElementsByClassName("showcase")
+const arrow = document.getElementsByClassName("arrowicon")
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+function dark() {
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+        icocheck()
+    } else {
+        document.body.classList.toggle("dark-theme");
+        icocheck()
+    }
+}
+
+function icocheck(){
+    if (document.body.classList.contains('light-theme')){
+        modeicon[0].innerHTML = `dark_mode`
+    } else{
+        modeicon[0].innerHTML = `light_mode`
+    }
+}
+
+function menu(x){
+    x.classList.toggle("change")
+}
+
+function openNav(){
+    sidenav[0].classList.toggle("open")
+}
+
+function hidearrow(x){
+    arrow[x].style.cssText = 'color: blue;'
+}
+
+function showarrow(x){
+    arrow[x].style.cssText = 'color: white;'
+}
+
+function openbuyiphone(){
+    window.open("./buy-iphone14.html", "_self")
+}
