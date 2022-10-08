10d386344de3e8b5299d7789f565cdce98de6b33
GerzsonUwU <zsolt.andorko.111@gmail.com>
2022-10-08 23:39:46 +0200

    editing cookie system

--- js/store.js
+++ js/store.js
@@ -114,8 +114,44 @@ function cookiessetup(){
             expires = 'expires=';
         date.setTime(date.getTime() + 1);
         expires += date.toGMTString();
-        document.cookie = name + '=' + value + '; ' + expires + '; path=/';
+        document.cookie = name + '=' + value + '; ' + 'SameSite=None; secure;' + expires + '; path=/;';
     }
     
-    setCookie("itemsincart", "0")
+    setCookie("itemsincart", "1")
 }
+
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
